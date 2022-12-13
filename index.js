537538d9c35ae86219d229fb205da6301798d531
davey-mwaura <davidmburu50@gmail.com>
2022-12-14 02:32:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-/* Your Code Here */
+function createEmployeeRecord(firstName,familyName,title,payPerHour){
+let employee = {
+  firstName: '',
+  familyName: '',
+  title: '',
+  payPerHour: 0,
+  timeInEvents: [],
+  timeOutEvents: [],
+}
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecord(){
+   
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(){
+    const TimeIn = {
+        type: '',
+        hour: '',
+        date: '',
+    }
+}
+
+function createTimeOutEvent(){
+    const TimeOut = {
+        type: '',
+        hour: '',
+        date: '',
+    }
+
+function hoursWorkedOnDate(){
+
+}
+
+function wagesEarnedOnDate(){
+
+}
+function allWagesFor(){
+
+}
+function findEmployeeByFirstName(){
+
+}
+function calculatePayroll(){
+
+}
+}
+
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
